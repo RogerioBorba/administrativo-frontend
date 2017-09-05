0bf8bd59331cfb82c851a7dc0eed47269fe934a5
RogerioBorba <idehco3@gmail.com>
2017-09-04 21:28:39 -0300

    frontend

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    
+    <tipo-gasto></tipo-gasto>
+  </div>
+</template>
+
+<script>
+import TipoGasto from './components/TipoGasto'
+
+export default {
+  name: 'app',
+  components: {
+    'tipo-gasto': TipoGasto
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
