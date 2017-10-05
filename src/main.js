e5cf54d37b8fa0906d39ab82506b9b4b6e6cdb27
RogerioBorba <idehco3@gmail.com>
2017-10-05 20:30:31 -0300

    adjust for vuetify

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import Vuetify from 'vuetify';
 import axios from 'axios';
 import VueLocalStorage from 'vue-localstorage';
 import {config} from './components/config';
+
+import('../node_modules/vuetify/dist/vuetify.min.css')
 axios.defaults.baseURL = 'http://127.0.0.1:8000/controle/';
 axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
